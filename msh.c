aa95c76c53ea392f9fa1f6730f5bad26d4d7838f
Mariah <mariahgardner00@gmail.com>
2025-02-07 15:02:55 -0600

    v1: program termiates at q

--- msh.c
+++ msh.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// msh - maverick shell program
+// made by Mariah Gardner 1001576678
+// complied with gcc -o msh msh.c
 
 int main(int argc, char *argv[]) {
+
+    // track if program is on or off
+    int programOnOff = 1;
+
+    //prompt user for input
     printf("\n msh> ");
+
+    // loop until user enters 'q'
+    while(programOnOff) {
+        char command[100]; // buffer for user input
+        fgets(command, sizeof(command), stdin); // read user input
+
+        // check if user entered 'q'
+        if (command[0] == 'q') { 
+            programOnOff = 0; // terminate program if 'q' is entered
+        } else {
+            // else, do the command
+        }
+    }
     return 0;
 }
